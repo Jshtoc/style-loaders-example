2b0dd4bbd545ed403a7c68310e8ccd22f97927b5
Jshtoc <tkdqja0517@gmail.com>
2021-12-17 13:34:00 +0900

    className과 모듈.css적용

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+// import './App.css';
+// import "./App.scss";
+import styles from "./App.module.css";
+
+import Button from "./components/Button";
+
+console.log(styles);
+// {App: "App_App__2_wh8"
+// App-header: "App_App-header__1HFdb"
+// App-link: "App_App-link__1VJYO"
+// App-logo: "App_App-logo__13QiU"
+// App-logo-spin: "App_App-logo-spin__2Kotf"}
+// 모듈이란 전역적으로 오염되지 않도록 하는것.
+// {style['^&^^']} or {style.%DSA}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className={styles["App"]}>
+      <header className={styles["App-header"]}>
+        <img src={logo} className={styles["App-logo"]} alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button>Button</Button>
       </header>
     </div>
   );
